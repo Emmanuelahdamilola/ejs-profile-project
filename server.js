6fcd0df14cbb7a1f4f0848d11152d52a9a2937a3
emmanuelah-dev <emmanuelahdamilola@gmail.com>
2025-10-15 16:29:22 +0100

    Profile updated

--- server.js
+++ server.js
@@ -12,26 +12,24 @@ app.set('view engine', 'ejs');
 // Load profiles from JSON
 const profiles = require('./data.json');
 
-// Routes
 
-// Home / list of profiles
 app.get('/', (req, res) => {
   res.render('index', { profiles });
 });
 
-// Individual profile page
+
 app.get('/profile/:id', (req, res) => {
   const id = req.params.id;
   const profile = profiles.find(p => String(p.id) === id);
   if (!profile) {
-    // render 404 template for not found
+
     res.status(404).render('404', { path: req.path });
   } else {
     res.render('profile', { profile });
   }
 });
 
-// Catch-all 404 for other routes
+
 app.use((req, res) => {
   res.status(404).render('404', { path: req.path });
 });
